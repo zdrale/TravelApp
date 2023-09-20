aa93e2e273f35a867979f7416b2af8f92c157424
zdrale <nikolazd22@outlook.com>
2023-09-20 12:25:20 +0200

    Updated import link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Navbar from './Components/Navabr/Navbar'
-import '../dist/css/App.css'
-import Home from './Components/Home/Home'
-import Middle from './Components/Middle/Middle'
-import Destinations from './Components/Destinations/Destinations'
-import Portfolio from './Components/Portfolio/Portfolio'
-import Reviews from './Components/Reviews/Reviews'
-import Questions from './Components/Questions/Questions'
-import Subscribe from './Components/Subscribe/Subscribe'
-import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Navabr/Navbar';
+import '../dist/css/App.css';
+import Home from './Components/Home/Home';
+import Middle from './Components/Middle/Middle';
+import Destinations from './Components/Destinations/Destinations';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Reviews from './Components/Reviews/Reviews';
+import Questions from './Components/Questions/Questions';
+import Subscribe from './Components/Subscribe/Subscribe';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 
